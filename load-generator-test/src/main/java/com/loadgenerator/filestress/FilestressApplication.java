71d1657af2310b23e948eabd2cc60f5a9ea15455
hachangmin6 <tom5757tom@pusan.ac.kr>
2025-05-18 16:54:25 +0900

    add load generator test code using webflux

---
+++ load-generator-test/src/main/java/com/loadgenerator/filestress/FilestressApplication.java
@@ -0,0 +1,13 @@
+package com.loadgenerator.filestress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilestressApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilestressApplication.class, args);
+    }
+
+}
---
+++ load-generator-test/src/test/java/com/loadgenerator/filestress/FilestressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loadgenerator.filestress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilestressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
